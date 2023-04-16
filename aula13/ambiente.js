cadc3f0fc516c4e132f824d131a46aaee9eaa429
Paulo Lucena <paulinholucena76@gmail.com>
2023-04-16 20:40:44 +0100

    Modulo E curso JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// CONDIÇÃO NO INICIO
+var c = 1 
+while (c < 6) {
+    c++ // c = c + 1
+}
+
+
+// CONDIÇÃO NO FIM
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,3 @@
+for (var c = 1;c <= 5;c++) { //ESTRUTURA DE REPETIÇÃO COM VARIAVEL DE CONTROLE
+    console.log(c)
+}
